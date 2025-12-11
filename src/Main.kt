225b93e1e19512bbe3376cef5dd27211ffe07a14
AndreiC805 <condria.razvan@gmail.com>
2025-12-11 13:29:32 +0000

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -13,7 +13,6 @@ fun main() {
 
     machine.start()
 
-    val admin = Admin(id = 1, email = "admin@example.com", fullName = "Admin User")
     val admin = Admin(id = 1, email = "admin@example.com", fullName = "Admin User",password="1234")
 
     while (true) {
